e7d6f1ef4bb86c236f8c4474a0b12b19db43ddd1
Diva Syakirahana <168277757+dipwaaaa@users.noreply.github.com>
2024-09-10 14:43:38 +0700

    Add files via upload

---
+++ program tugaslab1.pas
@@ -0,0 +1,53 @@
+program tugaslab1
+uses crt;
+
+var
+name; string:
+umur; anakke; dr; shortint:
+
+begin
+    clrscr;
+
+    writeln('Halo! Aku Ave, asisten pribadi kalian.');
+    writeln('Aku akan nemenin kalian selama satu minggu ke depan.);
+    writeln()
+    writeln(   /\_/\ ');
+    writeln('=  0 . 0  =');
+    writeln('  /      \  ');
+    writein();
+
+    writeln('Tapi, sebelum itu, aku butuh bantuan kamu!');
+    writeln('Karena kita baru ketemu, aku belum tau siapa kamu.');
+
+    writein('Boleh aku tau siapa nama kamu?);
+    writein();
+    write(Namaku '); readln(nama);
+    writeln();
+    writeln('Wah, nama kamu keren banget! Salam kenal ya, ',nama'. Semoga kamu betah sama aku.');
+
+    writein('Sekarang, hm... boleh aku tau berapa umur kamu? ');
+    writeln();
+    wrlte('Umur aku '); read('umur');
+    writeln();
+    writeln("Wow, muda sekali... aku merasa tua... T__T");
+
+    writeln('Aku penasaran deh! Kamu yang semuda ini anak ke berapa?')
+    writeln();
+    write(Aku anak ke-); read(anakke); 
+    ('');
+    writeln('Dari berapa bersaudara?');
+    writeln();
+    read(dari);
+    writeln();
+    readln('Hooo, begitu ternyata.');
+    writeln();
+
+    writeln('Oke! Terima kasih atas data dirinya ya, 'nama'!');
+    writeln('Saat ini, aku belum menerima perintah baru dari tuanku.')
+    wtiteln('Jadi, aku pergi dulu, ya!');
+    wtiteln('Dadah! Sampai jumpa minggu depan!');
+    writein();
+    writeln(   /\_/\ ');
+    writeln('=  0 . 0  =');
+    writeln('  /      \  );
+end.
